2bfdd0abb1f2cbc7d755254c07d1ceb07634326c
Ryan Mulcahy <ryan@primeacademy.io>
2017-01-25 11:43:19 -0600

    Reformat and add post route

--- routes/books.js
+++ routes/books.js
@@ -1,46 +1,75 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var pg = require('pg');
+var pg = require("pg");
 
-var config = {
-  database: 'library'
-};
+var config = { database: "library" };
 
 // initialize connection Pool
 // think of as 'how I connect to DB'
 var pool = new pg.Pool(config);
 
-router.get('/', function(req, res){
-
+router.get("/", function(req, res) {
   // err - an error object, will be non-null if there was some error
   //       connecting to the DB. ex. DB not running, config is incorrect
-
   // client - used to make actual queries against DB
-
   // done - function to call when we are finished
   //        returns the connection back to the pool
   pool.connect(function(err, client, done) {
     if (err) {
-      console.log('Error connecting to DB', err);
+      console.log("Error connecting to DB", err);
       res.sendStatus(500);
       done();
     } else {
       // no error occurred, time to query
-
       // 1. sql string - the actual SQL query we want to running
       // 2. callback - function to run after the database gives us our result
       //               takes an error object and the result object as it's args
-      client.query('SELECT * FROM books;', function(err, result){
+      client.query("SELECT * FROM books;", function(err, result) {
         done();
         if (err) {
-          console.log('Error querying DB', err);
+          console.log("Error querying DB", err);
           res.sendStatus(500);
         } else {
-          console.log('Got info from DB', result.rows);
+          console.log("Got info from DB", result.rows);
           res.send(result.rows);
         }
-      })
+      });
+    }
+  });
+});
+
+router.post("/", function(req, res) {
+  // err - an error object, will be non-null if there was some error
+  //       connecting to the DB. ex. DB not running, config is incorrect
+  // client - used to make actual queries against DB
+  // done - function to call when we are finished
+  //        returns the connection back to the pool
+  pool.connect(function(err, client, done) {
+    if (err) {
+      console.log("Error connecting to DB", err);
+      res.sendStatus(500);
+      done();
+    } else {
+      // no error occurred, time to query
+      // 1. sql string - the actual SQL query we want to running
+      // 2. array of data - any data we want to pass to a parameterized statement
+      // 3. callback - function to run after the database gives us our result
+      //               takes an error object and the result object as it's args
+      client.query(
+        "INSERT INTO books (title, author, publication_date) VALUES ($1, $2, $3) RETURNING *;",
+        [ req.body.title, req.body.author, req.body.publication_date ],
+        function(err, result) {
+          done();
+          if (err) {
+            console.log("Error querying DB", err);
+            res.sendStatus(500);
+          } else {
+            console.log("Got info from DB", result.rows);
+            res.send(result.rows);
+          }
+        }
+      );
     }
   });
 });
